46f8e1ad8bafe124661fa3366c4a795d06e304cc
vaibhav-novice <75840016+vaibhav-novice@users.noreply.github.com>
2021-07-19 09:11:48 +0530

    initial commit

---
+++ 125. Valid Palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+           if(s == null || s.length() == 0 || s.length() == 1){
+       return true;
+   }
+    
+    int left = 0;
+    int right = s.length()-1 ;
+    s = s.toLowerCase();
+    
+    while(left < right){
+        if(!Character.isLetterOrDigit(s.charAt(left))){
+            left++;
+        }else if(!Character.isLetterOrDigit(s.charAt(right))){
+            right--;
+        }else if(s.charAt(left) != s.charAt(right)){
+            return false;
+        }else{
+            left++;
+            right--;
+        }
+        
+    }
+    
+    return true;
+    }
+}
